c278aaea3a2dc22ea805f953417aa8dc1ad5a842
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2024-01-15 13:02:15 +0000

    async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""Import wait_n to 2-measure_runtime."""
-import asyncio
+"""Import wait_n into 2-measure_runtime.py"""
+from typing import List
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """Measure the total execution time for wait_n(n, max_delay)."""
+def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
-    delay = wait_n(n, max_delay)
+    wait_n(n, max_delay)
     end_time = time.time()
     total_time = end_time - start_time
-    average_time = total_time / n
-    return average_time
+    return total_time / n
