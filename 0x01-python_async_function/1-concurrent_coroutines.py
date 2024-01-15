c8dcc95d3c6f200f757f35a0a928923c4cd26b5c
Adeosun Oluwaseyi Esther <deosundeol@gmail.com>
2024-01-15 12:28:50 +0100

    asynction

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Import wait_random from 0-basic_async_syntax."""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """ Spawn wait_random n times with max_delay."""
+    tasks = []
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+    return [await task for task in asyncio.as_completed(tasks)]
