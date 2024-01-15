eda7d855ad7c136f8aa55f85751dca154835ac03
Adeosun Oluwaseyi Esther <deosundeol@gmail.com>
2024-01-15 13:00:01 +0100

    changes

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,14 +3,12 @@
 import asyncio
 from typing import List
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawn wait_random n times with max_delay."""
-    delays = []
-    tasks = [wait_random(n, max_delay) for n in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delays.append(await task)
-    return delays
+    tasks = []
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+    return [await task for task in asyncio.as_completed(tasks)]
