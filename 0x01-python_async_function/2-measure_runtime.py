23c95cbc1b52e08b072ef3987c2bce72aaff52e8
Adeosun Oluwaseyi Esther <deosundeol@gmail.com>
2024-01-15 13:53:59 +0100

    async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,20 +6,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    """
-    Measure the total execution time for wait_n(n, max_delay).
-
-    :param n: Number of times to spawn wait_random.
-    :param max_delay: Maximum delay in seconds.
-    :return: Average time per iteration in seconds.
-    """
+    """Measure the total execution time for wait_n(n, max_delay)."""
     start_time = time.time()
-
-    # Asynchronously execute wait_n
-    await wait_n(n, max_delay)
-
+    delay = wait_n(n, max_delay)
     end_time = time.time()
     total_time = end_time - start_time
-
-    # Return the average time per iteration
-    return total_time / n
+    average_time = total_time / n
+    return average_time
