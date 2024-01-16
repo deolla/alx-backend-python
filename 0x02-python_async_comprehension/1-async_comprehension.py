76e3cee3d5c0e74390690b47c0e152b546a370f3
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-16 12:19:42 +0100

    async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Import async_generator and write a coroutine called async_comprehension"""
-import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
+
