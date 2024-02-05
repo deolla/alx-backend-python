49cfbb0eea3fa0d9d87bf6d5bf32afb3fd3e2349
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-05 20:30:09 +0100

    Unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,10 +21,10 @@ class TestAccessNestedMap(unittest.TestCase):
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
 
-    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"), "b")])
     def test_access_nested_map_exception(self, nested_map, path, exceptions):
         """A method to test that the method raises the correct exceptions."""
         with self.assertRaises(KeyError) as context:
             utils.access_nested_map(nested_map, path)
 
-        self.assertEqual(str(context.exception), exceptions)
+        self.assertEqual(exceptions, str(context.exception))
